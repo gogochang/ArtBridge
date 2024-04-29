cf4beb74cd2f470161c95d327a8846acdce233a5
ChangGyuKim <okpo2581@gmail.com>
2024-04-29 23:06:44 +0900

    UI : 메시지 페이지 스크롤 아이템 UI 추가

--- ArtBridge/Sources/MainTabbar/Message/MessageViewController.swift
+++ ArtBridge/Sources/MainTabbar/Message/MessageViewController.swift
@@ -7,9 +7,181 @@
 
 import UIKit
 
+fileprivate enum Section: Hashable {
+    case horizontal
+    case vertical
+}
+
+fileprivate enum Item: Hashable {
+    case category(Int)
+    case preview(Int)
+}
+
 final class MessageViewController: UIViewController {
     override func viewDidLoad() {
-        view.backgroundColor = .orange
         super.viewDidLoad()
+        setupViews()
+        initialLayout()
+        
+        setDataSource()
+        createSnapshot()
+    }
+    
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Item>?
+    
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.createLayout()).then {
+        
+        $0.register(
+            CategoryCollectionViewCell.self,
+            forCellWithReuseIdentifier: CategoryCollectionViewCell.id
+        )
+        
+        $0.register(
+            MessageCollectionViewCell.self,
+            forCellWithReuseIdentifier: MessageCollectionViewCell.id
+        )
+    }
+    
+    private let navBar = ArtBridgeNavBar().then {
+        $0.leftBtnItem.setImage(UIImage(systemName: "apple.logo"), for: .normal)
+        $0.rightBtnItem.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+    }
+}
+
+//MARK: - Layout
+extension MessageViewController {
+    private func setupViews() {
+        view.addSubviews([
+            navBar,
+            collectionView
+        ])
+    }
+    
+    private func initialLayout() {
+        view.backgroundColor = .white
+        
+        navBar.snp.makeConstraints {
+            $0.top.left.right.equalToSuperview()
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(navBar.snp.bottom)
+            $0.left.right.bottom.equalToSuperview()
+        }
+    }
+}
+
+//MARK: - CompositionalLayout
+extension MessageViewController {
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 20
+        return UICollectionViewCompositionalLayout(sectionProvider: { [weak self] sectionIndex, _ in
+            let section = self?.dataSource?.sectionIdentifier(for: sectionIndex)
+            
+            switch section {
+            case .horizontal:
+                return self?.createHorizontalSection()
+            case .vertical:
+                return self?.createVerticalSection()
+            default:
+                return self?.createHorizontalSection()
+            }
+            
+        },configuration: config)
+    }
+    
+    private func createHorizontalSection() -> NSCollectionLayoutSection {
+        // Item
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
+        
+        // Group
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .absolute(160),
+            heightDimension: .absolute(40)
+        )
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+        
+        return section
+    }
+    
+    private func createVerticalSection() -> NSCollectionLayoutSection {
+        // Item
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 8, trailing: 4)
+        
+        // Group
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(80)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
+    }
+}
+
+//MARK: - DataSource
+extension MessageViewController {
+    private func createSnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        
+        let buttonItems = [Item.category(1), Item.category(2), Item.category(3)]
+        let buttonSection = Section.horizontal
+        
+        let previewItems = [Item.preview(1), Item.preview(2), Item.preview(3), Item.preview(4), Item.preview(5),
+                            Item.preview(6), Item.preview(7), Item.preview(8), Item.preview(9), Item.preview(10)]
+        let previewSection = Section.vertical
+        
+        snapshot.appendSections([buttonSection, previewSection])
+        snapshot.appendItems(buttonItems, toSection: buttonSection)
+        snapshot.appendItems(previewItems, toSection: previewSection)
+        
+        dataSource?.apply(snapshot)
+    }
+    
+    private func setDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(
+            collectionView: collectionView,
+            cellProvider: { collectionView, indexPath, item in
+                
+                switch item {
+                case .category:
+                    let cell = collectionView.dequeueReusableCell(
+                        withReuseIdentifier: CategoryCollectionViewCell.id,
+                        for: indexPath
+                    ) as? CategoryCollectionViewCell
+                    
+                    return cell
+                    
+                case .preview:
+                    let cell = collectionView.dequeueReusableCell(
+                        withReuseIdentifier: MessageCollectionViewCell.id,
+                        for: indexPath
+                    ) as? MessageCollectionViewCell
+                    
+                    return cell
+                }
+                
+            }
+        )
+        
     }
 }
--- ArtBridge/Sources/MainTabbar/Message/View/MessageCollectionViewCell.swift
+++ ArtBridge/Sources/MainTabbar/Message/View/MessageCollectionViewCell.swift
@@ -1,14 +1,14 @@
 //
-//  PostCollectionViewCell.swift
+//  MessageCollectionViewCell.swift
 //  ArtBridge
 //
-//  Created by 김창규 on 4/28/24.
+//  Created by 김창규 on 4/29/24.
 //
 
 import UIKit
 
-final class PostCollectionViewCell: UICollectionViewCell {
-    static let id = "PostCollectionViewCell"
+final class MessageCollectionViewCell: UICollectionViewCell {
+    static let id = "MessageCollectionViewCell"
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,47 +22,42 @@ final class PostCollectionViewCell: UICollectionViewCell {
     
     private let imageView = UIImageView().then {
         $0.backgroundColor = .systemGray6
-        $0.layer.cornerRadius = 10
+        $0.layer.cornerRadius = 20
     }
     
-    private let titelLabel = UILabel().then {
-        $0.text = "Title"
+    private let userNameLabel = UILabel().then {
+        $0.text = "Post Title"
     }
     
-    private let subTitelLabel = UILabel().then {
-        $0.text = "SubTitle"
+    private let previewLabel = UILabel().then {
+        $0.text = "Post SubTitle"
     }
 }
 
 //MARK: - Layout
-extension PostCollectionViewCell {
+extension MessageCollectionViewCell {
     private func setupViews() {
         addSubviews([
             imageView,
-            titelLabel,
-            subTitelLabel
+            userNameLabel,
+            previewLabel
         ])
     }
     
     private func initialLayout() {
-        backgroundColor = .white
-        layer.cornerRadius = 10
-        layer.borderWidth = 1
-        layer.borderColor = UIColor.systemGray6.cgColor
-        
         imageView.snp.makeConstraints {
             $0.top.left.bottom.equalToSuperview().inset(8)
             $0.width.equalTo(imageView.snp.height)
         }
         
-        titelLabel.snp.makeConstraints {
+        userNameLabel.snp.makeConstraints {
             $0.bottom.equalTo(self.snp.centerY).offset(-8)
-            $0.right.equalToSuperview().inset(20)
+            $0.left.equalTo(imageView.snp.right).offset(16)
         }
         
-        subTitelLabel.snp.makeConstraints {
+        previewLabel.snp.makeConstraints {
             $0.top.equalTo(self.snp.centerY).offset(8)
-            $0.right.equalToSuperview().inset(20)
+            $0.left.equalTo(imageView.snp.right).offset(16)
         }
     }
 }
