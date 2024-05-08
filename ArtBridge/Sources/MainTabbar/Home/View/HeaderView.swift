da9637c718b0898b2fd9257d0437d04ca7c046a5
ChangGyuKim <okpo2581@gmail.com>
2024-05-08 23:08:15 +0900

    UI : 홈 화면 헤더뷰에 전체보기 버튼 UI 추가

--- ArtBridge/Sources/MainTabbar/Home/View/HeaderView.swift
+++ ArtBridge/Sources/MainTabbar/Home/View/HeaderView.swift
@@ -10,8 +10,20 @@ import UIKit
 final class HeaderView: UICollectionReusableView {
     static let id = "HeaderView"
     
+    //MARK: - UI
     private let titleLabel = UILabel().then {
         $0.font = .systemFont(ofSize: 20, weight: .bold)
+    }
+    
+    private let moreBtnLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 16, weight: .regular)
+        $0.textColor = .systemGray3
+        $0.text = "전체보기"
+    }
+    
+    private let rightIcon = UIImageView().then {
+        $0.image = UIImage(systemName: "chevron.right")
+        $0.tintColor = .systemGray3
     }
     
     override init(frame: CGRect) {
@@ -32,12 +44,26 @@ final class HeaderView: UICollectionReusableView {
 //MARK: - Layout
 extension HeaderView {
     private func setupViews() {
-        addSubviews([titleLabel])
+        addSubviews([
+            titleLabel,
+            moreBtnLabel,
+            rightIcon
+        ])
     }
     
     private func initialLayout() {
         titleLabel.snp.makeConstraints {
             $0.top.left.right.equalToSuperview()
+        }
+        
+        moreBtnLabel.snp.makeConstraints {
+            $0.centerY.equalTo(titleLabel)
+            $0.right.equalTo(rightIcon.snp.left).offset(-4)
+        }
+        
+        rightIcon.snp.makeConstraints {
+            $0.centerY.equalTo(moreBtnLabel)
+            $0.right.equalToSuperview()
         }
     }
 }
