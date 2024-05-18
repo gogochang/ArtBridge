e6d92e08a63fd8e6df8522707173c92c32fb30e6
ChangGyuKim <okpo2581@gmail.com>
2024-05-18 10:26:25 +0900

    feat: "강사 상세보기"페이지 뒤로가기 기능 추가

--- ArtBridge/Sources/MainTabbar/Home/TutorDetail/DetailTutorViewController.swift
+++ ArtBridge/Sources/MainTabbar/Home/TutorDetail/DetailTutorViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 import RxSwift
 
+fileprivate enum Section: Hashable {
+    case banner
+    case title
+    case profile
+    case detailInfo
+    case description
+}
+
+fileprivate enum Item: Hashable {
+    case bannerItem
+}
+
 final class DetailTutorViewController: UIViewController {
     //MARK: - Properties
     private let viewModel: DetailTutorViewModel
     private let disposeBag: DisposeBag = DisposeBag()
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Item>?
+    
+    //MARK: - UI
+    private let navBar = ArtBridgeNavBar().then {
+        $0.leftBtnItem.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        $0.rightBtnItem.setImage(UIImage(systemName: "bell"), for: .normal)
+    }
+    
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.createLayout()).then {
+        $0.backgroundColor = .orange
+    }
     
     //MARK: - Init
     init(viewModel: DetailTutorViewModel) {
@@ -26,6 +49,79 @@ final class DetailTutorViewController: UIViewController {
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
+        setupViews()
+        initialLayout()
+        
+        viewModelInput()
+    }
+    
+    private func viewModelInput() {
+        navBar.leftBtnItem.rx.tap
+            .bind(to: viewModel.inputs.backward)
+            .disposed(by: disposeBag)
     }
 }
+
+//MARK: - Layout
+extension DetailTutorViewController {
+    private func setupViews() {
+        view.addSubviews([
+            navBar,
+            collectionView
+        ])
+    }
+    
+    private func initialLayout() {
+        self.view.backgroundColor = .white
+        navBar.snp.makeConstraints {
+            $0.top.left.right.equalToSuperview()
+        }
+    }
+}
+
+//MARK: - CompositionalLayout
+extension DetailTutorViewController {
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        return UICollectionViewCompositionalLayout(sectionProvider: { [weak self] sectionIndex, _ in
+            let section = self?.dataSource?.sectionIdentifier(for: sectionIndex)
+            switch section {
+            case .banner:
+                return self?.createBannerSection()
+            case .title:
+                return nil
+            case .profile:
+                return nil
+            case .detailInfo:
+                return nil
+            case .description:
+                return nil
+            default:
+                return nil
+            }
+        
+        },configuration: config)
+    }
+    
+    private func createBannerSection() -> NSCollectionLayoutSection {
+        //Item
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        //Group
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(400)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        //Section
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
+    }
+}
+
