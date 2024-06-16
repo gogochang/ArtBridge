acfa0cdd4d653733a8e7a474d8abc823b705b4b9
ChangGyuKim <okpo2581@gmail.com>
2024-06-16 14:21:07 +0900

    UI : "알람" 페이지 아이템 UI 추가     - 아이템 목록 목업 레이아웃 추가

--- ArtBridge/Sources/MainTabbar/Alarm/AlarmViewController.swift
+++ ArtBridge/Sources/MainTabbar/Alarm/AlarmViewController.swift
@@ -8,15 +8,28 @@
 import UIKit
 import RxSwift
 
+fileprivate enum Section: Hashable {
+    case vertical
+}
+
+fileprivate enum Item: Hashable {
+    case alarmItem(String)
+}
+
 final class AlarmViewController: UIViewController {
     //MARK: - Properties
     private let viewModel: AlarmViewModel
     private var disposeBag = DisposeBag()
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Item>?
     
     //MARK: - UI
     private let navBar = ArtBridgeNavBar().then {
         $0.leftBtnItem.setImage(UIImage(systemName: "chevron.left"), for: .normal)
         $0.title.text = "알람"
+    }
+    
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.createLayout()).then {
+        $0.register(AlarmCollectionViewCell.self, forCellWithReuseIdentifier: AlarmCollectionViewCell.id)
     }
     
     //MARK: - Init
@@ -34,6 +47,9 @@ final class AlarmViewController: UIViewController {
         setupViews()
         initialLayout()
         
+        setDataSource()
+        createSnapshot()
+        
         viewModelInput()
     }
     
@@ -49,7 +65,8 @@ final class AlarmViewController: UIViewController {
 extension AlarmViewController {
     private func setupViews() {
         view.addSubviews([
-            navBar
+            navBar,
+            collectionView
         ])
     }
     
@@ -59,5 +76,78 @@ extension AlarmViewController {
         navBar.snp.makeConstraints {
             $0.top.left.right.equalToSuperview()
         }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(navBar.snp.bottom)
+            $0.left.bottom.right.equalToSuperview()
+        }
+    }
+}
+
+//MARK: - CompositionalLayout
+extension AlarmViewController {
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout(sectionProvider: { [weak self] sectionIndex, _ in
+            let section = self?.dataSource?.sectionIdentifier(for: sectionIndex)
+            
+            switch section {
+            case .vertical:
+                return self?.createVerticalSection()
+            default:
+                return nil
+            }
+        })
+    }
+    
+    private func createVerticalSection() -> NSCollectionLayoutSection {
+        // Item
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(44)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 8, trailing: 4)
+        
+        // Group
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(44)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+}
+
+//MARK: - DataSource
+extension AlarmViewController {
+    private func createSnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        
+        let alarmItems = [Item.alarmItem("1"),Item.alarmItem("2"),Item.alarmItem("3")]
+        let alarmSection = Section.vertical
+        
+        snapshot.appendSections([alarmSection])
+        snapshot.appendItems(alarmItems)
+        
+        dataSource?.apply(snapshot)
+    }
+    
+    private func setDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(
+            collectionView: collectionView,
+            cellProvider: { collectionView, indexPath, item in
+                switch item {
+                case .alarmItem(let text):
+                    let cell = collectionView.dequeueReusableCell(
+                        withReuseIdentifier: AlarmCollectionViewCell.id,
+                        for: indexPath
+                    ) as? AlarmCollectionViewCell
+                    return cell
+                }
+            }
+        )
     }
 }
---
+++ ArtBridge/Sources/MainTabbar/Alarm/Cells/AlarmCollectionViewCell.swift
@@ -0,0 +1,103 @@
+//
+//  AlarmCollectionViewCell.swift
+//  ArtBridge
+//
+//  Created by 김창규 on 6/16/24.
+//
+
+import UIKit
+
+final class AlarmCollectionViewCell: UICollectionViewCell {
+    static let id = "AlarmCollectionViewCell"
+    
+    //MARK: - UI
+    private let titleLabel = UILabel().then {
+        $0.text = "알람 제목"
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+    }
+    
+    private let dateLabel = UILabel().then {
+        $0.text = "2024.06.14"
+        $0.textColor = .systemGray3
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+    }
+    
+    private let categoryContainer = UIView().then {
+        $0.backgroundColor = .systemGray6
+        $0.layer.cornerRadius = 10
+    }
+    
+    private let categoryLabel = UILabel().then {
+        $0.text = "카테고리"
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+    }
+    
+    private let textLabel = UILabel().then {
+        $0.text = "알람내용알람내용알람내용알람내용알람내용"
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        $0.numberOfLines = 1
+    }
+    
+    private let lineView = UIView().then {
+        $0.backgroundColor = .systemGray6
+    }
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        initialLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+//MARK: - Layout
+extension AlarmCollectionViewCell {
+    private func setupViews() {
+        addSubviews([
+            titleLabel,
+            categoryContainer,
+            textLabel,
+            dateLabel,
+            lineView
+        ])
+        
+        categoryContainer.addSubview(categoryLabel)
+    }
+    
+    private func initialLayout() {
+        categoryContainer.snp.makeConstraints {
+            $0.top.left.equalToSuperview().inset(8)
+        }
+        
+        categoryLabel.snp.makeConstraints {
+            $0.top.left.bottom.right.equalToSuperview().inset(4)
+            $0.center.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.left.equalTo(categoryLabel.snp.right).offset(8)
+            $0.centerY.equalTo(categoryLabel)
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.left.equalTo(titleLabel.snp.right).offset(8)
+            $0.centerY.equalTo(titleLabel)
+        }
+        
+        textLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+            $0.left.equalTo(categoryLabel)
+        }
+        
+        lineView.snp.makeConstraints {
+            $0.top.equalTo(textLabel.snp.bottom).offset(8)
+            $0.left.bottom.right.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+    }
+}
